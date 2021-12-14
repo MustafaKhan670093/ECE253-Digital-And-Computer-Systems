1c3b374a277f773edc118d02ae97654719119363
Mustafa Khan <48969731+MustafaKhan670093@users.noreply.github.com>
2021-12-14 10:56:03 -0500

    ARM Assembly

--- Lab 8/part2.s
+++ Lab 8/part2.s
@@ -1,29 +1,40 @@
+.text
 .global _start
-_start:
-    PUSH {R0-R7, LR}
-bsort_next:                     // Check for a sorted array
-    MOV     R2,#0               // R2 = Current Element Number
-    MOV     R6,#0               // R6 = Number of swaps
-bsort_loop:                     // Start loop
-    ADD     R3,R2,#1            // R3 = Next Element Number
-    CMP     R3,R1               // Check for the end of the array
-    BGE     bsort_check         // When we reach the end, check for changes
-SWAP:
-    LDR     R4,[R7,R2,LSL #2]   // R4 = Current Element Value
-    LDR     R5,[R7,R3,LSL #2]   // R5 = Next Element Value
-    CMP     R4,R5               // Compare element values
-    STRGT   R5,[R7,R2,LSL #2]   // If R4 > R5, store current value at next
-    STRGT   R4,[R7,R3,LSL #2]   // If R4 > R5, Store next value at current
-    ADDGT   R6,R6,#1            // If R4 > R5, Increment swap counter
-    MOV     R2,R3               // Advance to the next element
-    ADDGT   R0,R0,#1	        // If R4 > R5, Increment swap counter in R0
-    CMP	    R0,#0		// Compare R0 with 0
-    STRGT   R0,#1		// If R0 > 0, then store 1 in R0
-    BEQ	    R0,#0		// If R0 = 0, then store 0 in R0
-    B       bsort_loop          // End loop
-bsort_check:                    // Check for changes
-    CMP     R6,#0               // Were there changes this iteration?
-    SUBGT   R1,R1,#1            // Optimization: skip last value in next loop
-    BGT     bsort_next          // If there were changes, do it again
-bsort_done:                     // Return
-    POP     {R0-R7,PC}          // Pop the registers off of the stack
+.global SWAP
+
+_start: LDR R12, =LIST // load list array
+        MOV R10, R12
+	LDR R11, [R12] //  
+  
+TRACKER:    
+	MOV R12, R10 
+        SUB R11, #1 
+        CMP R11, #0 
+        MOV R7, R11
+        BNE FOR
+
+END:    
+	B END
+    
+FOR:    MOV R2, #0
+        MOV R1, #0
+        MOV R0, #0
+        ADD R12, #4 // Shift to next number in array
+        ADD R0, R0, R12
+        CMP R7, #0 // If loop length is 0, perform no swaps
+        SUB R7, #1 
+        BEQ TRACKER
+        BL SWAP 
+        B FOR    
+
+SWAP:    
+	LDR R1, [R0] 
+        LDR R2, [R0, #4] 
+        CMP R1, R2  
+        STRGT R2, [R0]   
+        STRGT R1, [R0, #4] 
+        MOVLE R0, #0
+        MOVGT R0, #1 
+        MOV R1, #0
+        MOV R2, #0
+        MOV PC, LR
